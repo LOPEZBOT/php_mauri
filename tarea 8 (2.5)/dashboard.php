49090931cd3eb823a0c59b53b29c5fb4a945a9a3
LOPEZBOT <138363843+LOPEZBOT@users.noreply.github.com>
2023-09-28 16:54:57 -0400

    Add files via upload

---
+++ tarea 8 (2.5)/dashboard.php
@@ -0,0 +1,31 @@
+<?php session_start();?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+    <h2>Sesion abierta</h2>
+    <p>
+       <?php 
+       if(isset($_POST['nombre'])){
+        $_SESSION['nombre'] = $_POST['nombre'];
+        echo "Bienvenido,<b> ". $_POST['nombre']. "</b>";
+
+       }else{
+            if(isset($_SESSION['nombre'])){
+                echo "Has iniciado sesion como: ". $_SESSION['nombre'];
+            }else{
+        echo "Acceso Restringido";
+       }
+    }
+    ?>
+    </p>
+    <br>
+    <p><a href="login.php">Ir a la pagina inicial</a></p>  
+    <br>
+    <p><a href="logout.php">Cerrar Sesion</a></p>     
+</body>
+</html>
